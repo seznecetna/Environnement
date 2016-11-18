677542162cd74e8f6278b13df14d8ae4a4a927a6
root <root@debian-capgemini>
2016-11-18 15:34:29 +0100

    Hello world template + variable

--- src/Environnement/plateFormBundle/Controller/AdvertController.php
+++ src/Environnement/plateFormBundle/Controller/AdvertController.php
@@ -2,10 +2,16 @@
 
 namespace Environnement\plateFormBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdvertController {
-      public function indexAction() {
-      	     return new Response("Notre propre Hello World !");
-      }
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        $content = $this
+            ->get('templating')
+            ->render('EnvironnementplateFormBundle:Advert:index.html.twig', array('nom' => 'Jocelyn'));
+             return new Response($content);
+    }
 }
