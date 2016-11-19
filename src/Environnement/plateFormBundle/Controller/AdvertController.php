d8ae3bc24360aee3ee993c019daa507feab82748
root <root@debian-capgemini>
2016-11-19 16:28:30 +0100

    changement de la variable

--- src/Environnement/plateFormBundle/Controller/AdvertController.php
+++ src/Environnement/plateFormBundle/Controller/AdvertController.php
@@ -11,7 +11,7 @@ class AdvertController extends Controller
     {
         $content = $this
             ->get('templating')
-            ->render('EnvironnementplateFormBundle:Advert:index.html.twig', array('nom' => 'Jocelyn'));
+            ->render('EnvironnementplateFormBundle:Advert:index.html.twig', array('nom' => 'Jocelyn #Biniou '));
              return new Response($content);
     }
 }
